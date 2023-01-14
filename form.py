7c877015274659b497887fedad277686991598fb
Samtxx <sompokefan@gmail.com>
2023-01-14 20:30:23 +0530

    removed unnecessary comments, and cleanup

--- form.py
+++ form.py
@@ -14,40 +14,7 @@ PE1_WEIGHTAGE = 0.1
 PE2_WEIGHTAGE = 0.1
 PE3_WEIGHTAGE = 0.3
 PEY_WEIGHTAGE = 0.4
-# Cases to handle: hindiHL, spanishABHL, frenchABHL, spanishBHL, frenchBHL, ESS
-# englishHL = [12, 25, 35, 49, 61, 76, 100]
-# englishSL = [9, 22, 34, 45, 59, 76, 100]
-# spanishBSL = [5, 11, 24, 40, 56, 74, 100]
-# frenchBSL = [9, 20, 32, 48, 64, 79, 100]
-# hindiBSL = [8, 19, 34, 48, 63, 78, 100]
-# spanishABSL = [11, 22, 34, 49, 62, 77, 100]
-# frenchABSL = [10, 22, 36, 52, 65, 81, 100]
-# math_aaHL = [3, 11, 23, 37, 52, 68, 100]
-# math_aiHL = [3, 9, 18, 32, 46, 61, 100]
-# math_aaSL = [3, 11, 22, 39, 57, 71, 100]
-# math_aiSL = [3, 12, 24, 38, 54, 69, 100]
-# physicsHL = [13, 19, 28, 38, 53, 68, 100]
-# physicsSL = [9, 14, 25, 36, 49, 62, 100]
-# chemistryHL = [14, 19, 34, 49, 66, 79, 100]
-# chemistrySL = [4, 9, 23, 45, 60, 75, 100]
-# biologyHL = [15, 23, 33, 46, 58, 75, 100]
-# biologySL = [11, 21, 30, 43, 58, 74, 100]
-# psychologyHL = [7, 16, 28, 41, 52, 65, 100]
-# psychologySL = [5, 11, 18, 35, 49, 65, 100]
-# digital_societiesHL = [29, 39, 49, 59, 71, 84, 100]
-# digital_societiesSL = [29, 39, 49, 59, 71, 84, 100]
-# global_politicsHL = [9, 19, 28, 39, 50, 61, 100]
-# global_politicsSL = [6, 18, 28, 37, 48, 58, 100]
-# visual_artsHL = [7, 17, 33, 47, 62, 76, 100]
-# visual_artsSL = [8, 17, 31, 45, 62, 76, 100]
-# economicsHL = [8, 17, 26, 39, 53, 66, 100]
-# economicsSL = [9, 19, 30, 43, 58, 71, 100]
-# business_managementHL = [10, 24, 33, 45, 54, 64, 100]
-# business_managementSL = [9, 17, 26, 39, 53, 66, 100]
-# computer_scienceHL = [10, 15, 21, 34, 48, 62, 100]
-# computer_scienceSL = [5, 10, 26, 36, 48, 61, 100]
-# essSL = [29, 39, 49, 59, 71, 84, 100]
-# essHL = [29, 39, 49, 59, 71, 84, 100]
+
 
 
 englishHL = [12, 28, 45, 58, 68, 82, 100]
@@ -221,22 +188,6 @@ def accountDone():
         else:
             return render_template('newAccount.html', message = "Please use an official CHIREC email id to register")
     
-    # for i in data:
-    #     em, pwd = i.split('=') 
-    #     if em == email:
-    #         return render_template('newAccount.html', message = "This account already exists")
-    #     else:
-    #         data.write(f'{email}={password}\n')
-    #         return render_template('accountDone.html')
-            
-    # try:
-        
-        
-    #     cursor.execute(f'insert into password (Email, Password) values(\'{str(email)}\', \'{str(password)}\')')
-    #     db.commit()
-    #     return render_template('accountDone.html')
-    # except mysql.connector.IntegrityError:
-        # return render_template('newAccount.html', message = "This account already exists")
 
 
 
@@ -245,9 +196,7 @@ def input():
     if request.method == 'POST':
         email = str(request.form['email'])
         print(email)
-        # cursor.execute(f'select Password from password where email=\'{(str(email))}\'')
-        # res = str(cursor.fetchall())
-        # res = res[3:-4]
+
         try:
             file_pass = password_data[email]
             print(file_pass)
@@ -336,44 +285,6 @@ def index():
     if ((subject6 == 'spanishB' or subject6 == 'frenchB' or subject6 == 'hindiB' or subject6 == 'spanishAB' or subject6 == 'frenchAB') and (level6 == 'HL')):
         return render_template('inputForm.html', message = "Invalid subject-level combination entered in subject 6")
     
-    # subject1 = request.form['Subject 1'] if request.form['Subject 1'] else None
-    # level1 = request.form['Subject 1 Level'] if request.form['Subject 1 Level'] else None
-    # pe1_1 = int(request.form['Subject 1 PE1']) if request.form['Subject 1 PE1'] else None
-    # pe2_1 = int(request.form['Subject 1 PE2']) if request.form['Subject 1 PE2'] else None
-    # pe3_1 = int(request.form['Subject 1 PE3']) if request.form['Subject 1 PE3'] else None
-    # pey_1 = int(request.form['Subject 1 PEY2']) if request.form['Subject 1 PEY2'] else None
-    # print(request.form['Subject 2'])
-    # subject2 = request.form['Subject 2'] if request.form['Subject 2'] else None
-    # print(subject2)
-    # level2 = request.form['Subject 2 Level'] if request.form['Subject 2 Level'] else None
-    # pe1_2 = int(request.form['Subject 2 PE1']) if request.form['Subject 2 PE1'] else None
-    # pe2_2 = int(request.form['Subject 2 PE2']) if request.form['Subject 2 PE2'] else None
-    # pe3_2 = int(request.form['Subject 2 PE3']) if request.form['Subject 2 PE3'] else None
-    # pey_2 = int(request.form['Subject 2 PEY2']) if request.form['Subject 2 PEY2'] else None
-    # subject3 = request.form['Subject 3'] if request.form['Subject 3'] else None
-    # level3 = request.form['Subject 3 Level'] if request.form['Subject 3 Level'] else None
-    # pe1_3 = int(request.form['Subject 3 PE1']) if request.form['Subject 3 PE1'] else None
-    # pe2_3 = int(request.form['Subject 3 PE2']) if request.form['Subject 3 PE2'] else None
-    # pe3_3 = int(request.form['Subject 3 PE3']) if request.form['Subject 3 PE3'] else None
-    # pey_3 = int(request.form['Subject 3 PEY2']) if request.form['Subject 3 PEY2'] else None
-    # subject4 = request.form['Subject 4'] if request.form['Subject 4'] else None
-    # level4 = request.form['Subject 4 Level'] if request.form['Subject 4 Level'] else None
-    # pe1_4 = int(request.form['Subject 4 PE1']) if request.form['Subject 4 PE1'] else None
-    # pe2_4 = int(request.form['Subject 4 PE2']) if request.form['Subject 4 PE2'] else None
-    # pe3_4 = int(request.form['Subject 4 PE3']) if request.form['Subject 4 PE3'] else None
-    # pey_4 = int(request.form['Subject 4 PEY2']) if request.form['Subject 4 PEY2'] else None
-    # subject5 = request.form['Subject 5'] if request.form['Subject 5'] else None
-    # level5 = request.form['Subject 5 Level'] if request.form['Subject 5 Level'] else None
-    # pe1_5 = int(request.form['Subject 5 PE1']) if request.form['Subject 5 PE1'] else None
-    # pe2_5 = int(request.form['Subject 5 PE2']) if request.form['Subject 5 PE2'] else None
-    # pe3_5 = int(request.form['Subject 5 PE3']) if request.form['Subject 5 PE3'] else None
-    # pey_5 = int(request.form['Subject 5 PEY2']) if request.form['Subject 5 PEY2'] else None
-    # subject6 = request.form['Subject 6'] if request.form['Subject 6'] else None
-    # level6 = request.form['Subject 6 Level'] if request.form['Subject 6 Level'] else None
-    # pe1_6 = int(request.form['Subject 6 PE1']) if request.form['Subject 6 PE1'] else None
-    # pe2_6 = int(request.form['Subject 6 PE2']) if request.form['Subject 6 PE2'] else None
-    # pe3_6 = int(request.form['Subject 6 PE3']) if request.form['Subject 6 PE3'] else None
-    # pey_6 = int(request.form['Subject 6 PEY2']) if request.form['Subject 6 PEY2'] else None
 
 
     form_values = {}
